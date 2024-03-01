2d5616c653924d97321a0b94a562aebd678c7846
Zmizet <garagedweller96@gmail.com>
2024-03-01 10:22:22 -0800

    fix for evocation

--- mage/routine.lua
+++ mage/routine.lua
@@ -3,12 +3,13 @@ local mage = project.mage.aoe
 local player = awful.player
 local settings = project.settings
 local player, target = awful.player, awful.target
-
+local channeling = ChannelInfo()
+if channeling then return end
 print("[|cffFF6B33Zmizet|r AoE |cff3FC7EBMage|r]")
 
 mage:Init(function()
     if player.mounted then return end 
-
+    if channeling then return end
     --beneficial
     Intellect()
     FrostShield()
